8c464117a0e22a5c146f519e81374579c694510b
xuanhai0913 <xuanhai0913750452@gmail.com>
2025-06-05 16:04:48 +0700

    Test

--- SPRM.Business/obj/Debug/net9.0/SPRM.Business.AssemblyInfo.cs
+++ SPRM.Business/obj/Debug/net9.0/SPRM.Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPRM.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a0e68464fe0096354599c17fe629f9239babd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPRM.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPRM.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SPRM.Data/obj/Debug/net9.0/SPRM.Data.AssemblyInfo.cs
+++ SPRM.Data/obj/Debug/net9.0/SPRM.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPRM.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a0e68464fe0096354599c17fe629f9239babd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPRM.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPRM.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
